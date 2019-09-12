22dc4db627bc5749b5e9e9635cbb7fa6cf226393
aldinofrizal <aldinofrizal@gmail.com>
2019-09-12 18:01:49 +0700

    family 100

--- index.js
+++ index.js
@@ -11,7 +11,22 @@ function sleep(milliseconds) {
 
 function match_data(parent_file, children_file) {
   // Code here
+  fs.readFile(parent_file, 'utf8', (err, dataParent) => {
+    sleep(1000)
+    fs.readFile(children_file, 'utf8', (err2, dataChild) => {
+      let dataParent_ = JSON.parse(dataParent)
+      let dataChildern_ = JSON.parse(dataChild)
+      dataParent_.forEach(parentObject => {
+          parentObject.childern = []
+          dataChildern_.forEach(childObject => {
+            if(childObject.family === parentObject.last_name)parentObject.childern.push(childObject.full_name)
+          })
+      });
+      console.log(dataParent_)
+    })
+  })
+
 }
 
 match_data('./parents.json', './children.json')
-console.log("Notification : Data sedang diproses !");
+// console.log("Notification : Data sedang diproses !");
