5653bacb5680692b5cfa924be4cb54463df9384f
Panji SN <panji1595@gmail.com>
2019-09-12 16:50:24 +0700

    Update tugas

--- index.js
+++ index.js
@@ -10,7 +10,28 @@ function sleep(milliseconds) {
 }
 
 function match_data(parent_file, children_file) {
-  // Code here
+  
+  
+  fs.readFile(parent_file,function(err,data){
+    let arrParent = JSON.parse(data)
+    sleep(5000)
+    fs.readFile(children_file,function(err,data2){
+      let arrChildren = JSON.parse(data2);
+      sleep(5000)
+      for (let i = 0; i < arrParent.length; i++){
+        let arrAnak = [];
+        for(let j = 0; j < arrChildren.length;j++){
+          if(arrChildren[j].family== arrParent[i].last_name){
+            arrAnak.push(arrChildren[j].full_name);
+          }
+        }
+        arrParent[i].children = arrAnak;
+        sleep(5000)
+      }
+      console.log(arrParent)
+      
+    })
+  })
 }
 
 match_data('./parents.json', './children.json')
