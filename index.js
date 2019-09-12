03f1325e6a486a753a68f67068b33ce808db68cc
anggabanny <anggabanny@gmail.com>
2019-09-12 17:05:31 +0700

    update

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ function sleep(milliseconds) {
 
 function match_data(parent_file, children_file) {
   // Code here
+  fs.readFile(parent_file,
+    function(err,data){
+      let dataList = JSON.parse(data)
+      // console.log(dataList);
+      for(let i = 0 ; i < dataList.length;i++){
+        dataList[i]['children'] = []
+        fs.readFile(children_file,
+          function(err,data1){
+            let dataChild = JSON.parse(data1)
+            // console.log(dataList);
+            for(let j = 0 ; j < dataChild.length; j++){
+              if (dataChild[j].family == dataList[i].last_name) {
+                dataList[i]['children'].push(dataChild[j].full_name)
+              }
+            }
+            console.log(dataList);
+            sleep(500)
+        })
+      }
+  })
 }
 
 match_data('./parents.json', './children.json')
