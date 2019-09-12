417d2d19d2ba2a5ecd3e72ede4a23de1c172c072
hoandreas <andreas_ho@hotmail.com>
2019-09-12 18:38:15 +0700

    submit callback multiple file

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ function sleep(milliseconds) {
 
 function match_data(parent_file, children_file) {
   // Code here
+  fs.readFile(parent_file, 'utf8', function(err, strParents) {
+    fs.readFile(children_file, 'utf8', function(err, strChildren) {
+      let parentsList = JSON.parse(strParents);
+      let childrenList = JSON.parse(strChildren);
+      for(let i = 0; i < parentsList.length; i++) {
+        parentsList[i].children = [];
+        for(let j = 0; j < childrenList.length; j++) {
+          if(parentsList[i].last_name === childrenList[j].family) {
+            parentsList[i].children.push(childrenList[j].full_name);
+          }
+        }
+        console.log(parentsList[i]);
+        sleep(5000);
+      }
+    });
+  });
 }
 
 match_data('./parents.json', './children.json')
