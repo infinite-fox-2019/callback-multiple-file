55e4214a36209729ad82f0a0fd99e39a88b653b4
ericsudhartio <*ericsudhartio2509@gmail.com>
2019-09-12 18:10:41 +0700

    fix

--- index.js
+++ index.js
@@ -14,20 +14,24 @@ function match_data(parent_file, children_file) {
     fs.readFile ( parent_file , 'utf8' , function ( err , parents ) {
       let parseParents = JSON.parse ( parents );
       for ( let i=0 ; i<parseParents.length; i++ ) {
-        fs.readFile ( children_file , 'utf8' , function ( err , childrens ) {
-          parseParents[i]['children'] = [];
-          let parseChildrens = JSON.parse ( childrens );
-          for ( let j=0; j<parseChildrens.length; j++ ) {
-            if ( parseChildrens[j].family == parseParents[i].last_name ) {
-              parseParents[i]['children'].push( parseChildrens[j].full_name );
-            }
-          }
-          console.log ( parseParents );
-          sleep(200);
-        })
+        parseParents[i]['children'] = [];
       }
+      fs.readFile ( children_file , 'utf8' , function ( err , childrens ) {
+      let parseChildrens = JSON.parse ( childrens );
+      for ( let i=0 ; i<parseParents.length; i++ ) {
+        for ( let j=0; j<parseChildrens.length; j++ ) {
+          if ( parseChildrens[j].family == parseParents[i].last_name ) {
+            parseParents[i]['children'].push( parseChildrens[j].full_name );
+          }
+        }
+      }
+      console.log ( parseParents );
+      // sleep(200);
+    })
     })
 }
 
 match_data('./parents.json', './children.json')
+
+// console.log( test[0].id )
 console.log("Notification : Data sedang diproses !");
