766105f63fd671a4859caff2a4f5a8f3bf85f12b
adityain98 <adityain98@gmail.com>
2019-09-12 16:07:58 +0700

    selesai

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const parent_file = './parent.json'
+const children_file = './children.json'
 
 function sleep(milliseconds) {
   var start = new Date().getTime();
@@ -10,7 +12,30 @@ function sleep(milliseconds) {
 }
 
 function match_data(parent_file, children_file) {
-  // Code here
+  fs.readFile(parent_file, function(err, dataParentJSON){
+    sleep(5000)
+    fs.readFile(children_file, function(err,dataChildrenJSON){
+      sleep(5000)
+      let arrParent = [];
+      let arrChildren = []
+      let dataParent = JSON.parse(dataParentJSON);
+      let dataChildren = JSON.parse(dataChildrenJSON);
+    
+      for(let i = 0; i<dataParent.length; i++){
+        
+        arrChildren = []
+        for(let j = 0; j<dataChildren.length; j++){
+          if(dataParent[i].last_name == dataChildren[j].family){
+            arrChildren.push(dataChildren[j].full_name)
+          }
+        }
+        dataParent[i].children = arrChildren;
+        arrParent.push(dataParent[i])
+      }
+      console.log(arrParent);
+      
+    })
+  })
 }
 
 match_data('./parents.json', './children.json')
