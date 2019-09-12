23f858f5f836a3b05d0383fca9531e5e635dc275
Kevin Anantha <kevin.anantha@gmail.com>
2019-09-12 16:00:20 +0700

    :anchor:

--- index.js
+++ index.js
@@ -10,7 +10,32 @@ function sleep(milliseconds) {
 }
 
 function match_data(parent_file, children_file) {
-  // Code here
+  fs.readFile(parent_file, 'utf8', (err, parents) => {
+    if (parents) {
+      const parentsData = JSON.parse(parents)
+
+      fs.readFile(children_file, 'utf8', (err, childrens) => {
+        if (childrens) {
+          const childrensData = JSON.parse(childrens)
+
+          for (let i = 0; i < parentsData.length; i++) {
+            parentsData[i].children = []
+            for (let j = 0; j < childrensData.length; j++) {
+              if (parentsData[i].last_name == childrensData[j].family) {
+                parentsData[i].children.push(childrensData[j].full_name)
+              }
+            }
+          }
+
+          console.log(parentsData)
+        } else {
+          console.log(`Error: ${err.message}`)
+        }
+      })
+    } else {
+      console.log(`Error: ${err.message}`)
+    }
+  })
 }
 
 match_data('./parents.json', './children.json')
