2e0aad97af34ee37becb5cf2043c23493f2db22a
ericsudhartio <*ericsudhartio2509@gmail.com>
2019-09-12 15:32:04 +0700

    done

--- index.js
+++ index.js
@@ -6,11 +6,27 @@ function sleep(milliseconds) {
     if ((new Date().getTime() - start) > milliseconds) {
       break;
     }
+
   }
 }
 
 function match_data(parent_file, children_file) {
-  // Code here
+    fs.readFile ( parent_file , 'utf8' , function ( err , parents ) {
+      let parseParents = JSON.parse ( parents );
+      for ( let i=0 ; i<parseParents.length; i++ ) {
+        fs.readFile ( children_file , 'utf8' , function ( err , childrens ) {
+        parseParents[i]['children'] = [];
+          let parseChildrens = JSON.parse ( childrens );
+          for ( let j=0; j<parseChildrens.length; j++ ) {
+            if ( parseChildrens[j].family == parseParents[i].last_name ) {
+              parseParents[i]['children'].push( parseChildrens[j].full_name );
+              console.log ( parseParents );
+              sleep(1000);
+            }
+          }
+        })
+      }
+    })
 }
 
 match_data('./parents.json', './children.json')
