380672a7ffbcc398b8159ed299d8b8b41ba6dfc7
risanto <potallegta@gmail.com>
2019-09-12 17:56:09 +0700

    Callback Read Multiple File: DONE

--- index.js
+++ index.js
@@ -10,7 +10,33 @@ function sleep(milliseconds) {
 }
 
 function match_data(parent_file, children_file) {
-  // Code here
+  fs.readFile(parent_file, 'utf8', (err, parent_data) => {
+    if (err) {
+        console.log("File read failed:", err)
+        return
+      }
+    let parents = JSON.parse(parent_data);
+    sleep(5000)
+
+    fs.readFile(children_file, 'utf8', (err, children_data) => {
+      if (err) {
+        console.log("File read failed:", err)
+        return
+        } 
+      let children = JSON.parse(children_data);
+      sleep(5000)
+
+      for(let i = 0; i < parents.length; i++) {
+        parents[i]['children'] = []
+        for (let j = 0; j < children.length; j++) {
+          if (parents[i]['last_name'] == children[j]['family']) {
+            parents[i]['children'].push(children[j]['full_name'])
+          }
+        }
+      }
+      console.log(parents);
+    })
+  })
 }
 
 match_data('./parents.json', './children.json')
